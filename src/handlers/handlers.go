11ea1aa2c262ca94b887f0814f64626a7ae9654b
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-12-10 10:06:34 +0100

    chore: adding query and response writing

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
 
@@ -75,5 +76,51 @@ func RequestHandler(responseWriter http.ResponseWriter, request *http.Request) {
 		logger.Warning("incoming request did not contain the needed query parameters")
 		helpers.SendRequestError(e.MissingQueryParameter, responseWriter)
 		return
+	}
+
+	// get the modelid and instance id
+	modelID := request.URL.Query().Get("modelID")
+	instanceID := request.URL.Query().Get("instanceID")
+
+	// Prepare a sql query
+	query := `SELECT data #>> '{}' FROM bim_models.instances WHERE model = $1 AND id = $2`
+
+	// execute the query on the database
+	instanceRows, queryError := vars.PostgresConnection.Query(query, modelID, instanceID)
+
+	if queryError != nil {
+		logger.WithError(queryError).Error(
+			"An error occurred while querying the database for the bim model" +
+				" information",
+		)
+		helpers.SendRequestError(e.DatabaseQueryError, responseWriter)
+		return
+	}
+	var instances []interface{}
+
+	for instanceRows.Next() {
+		var instanceJSON []byte
+		scanError := instanceRows.Scan(&instanceJSON)
+		var instance interface{}
+		err := json.Unmarshal(instanceJSON, &instance)
+		if err != nil {
+			logger.WithError(err).Error("unable to convert the returned rows due to an error")
+			helpers.SendRequestError(e.DatabaseQueryError, responseWriter)
+			return
+		}
+		if scanError != nil {
+			logger.WithError(scanError).Error("unable to scan the returned rows due to an error")
+			helpers.SendRequestError(e.DatabaseQueryError, responseWriter)
+			return
+		}
+
+		instances = append(instances, instance)
+	}
+
+	// Send back the response
+	responseWriter.Header().Set("Content-Type", "text/json")
+	encodingError := json.NewEncoder(responseWriter).Encode(instances)
+	if encodingError != nil {
+		logger.WithError(encodingError).Error("an error occurred while sending back the response")
 	}
 }
